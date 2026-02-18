8d6fa4945d333024853485814d0692b317fe49bc
sjm2504 <sarai.maitland@outlook.com>
2026-02-18 10:11:36 +0000

    Update server.js

--- server.js
+++ server.js
@@ -37,8 +37,8 @@ app.get('/submissions', (req, res) => {
     return res.status(403).json({ error: "Unauthorised" });
   }
 
-  const serverKey = "SaraiJasuM";
-  if (key !== serverKey) {
+  const serverKey = req.query.serverKey ;
+  if (serverKey !== "SaraiJasuM") {
     return res.status(403).json({ error: "Unauthorized" });
   }
 
@@ -55,6 +55,7 @@ app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 
 });
+
 
 
 
